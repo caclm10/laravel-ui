33fb56a54a115cfdef960b36cbe30351454e21ab
caclm10 <lewinxander@gmail.com>
2024-08-18 21:51:02 +0700

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import colors from "tailwindcss/colors"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+        success: colors.green,
+        danger: colors.red
+      },
+    },
+  },
+  plugins: [],
+}
+
